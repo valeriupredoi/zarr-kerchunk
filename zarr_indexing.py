2a3fe0e979b691dcdff924807a91907c3e6d1a74
Valeriu Predoi <valeriu.predoi@gmail.com>
2022-08-24 13:41:36 +0100

    getting there still

--- zarr_indexing.py
+++ zarr_indexing.py
@@ -7,11 +7,58 @@ from io import BytesIO
 from zarr.indexing import PartialChunkIterator
 
 
+dsize = (60, 404, 802)
+dchunks = (12, 80, 160)
+dvalue = 42.
+
+docstr = r"""
+This code uses zarr.indexing.PartialChunkIterator:
+
+Iterator to retrieve the specific coordinates of requested data
+    from within a compressed chunk.
+
+    Parameters
+    ----------
+    selection : tuple
+        tuple of slice objects to take from the chunk
+    arr_shape : shape of chunk to select data from
+
+    Attributes
+    -----------
+    arr_shape
+    selection
+
+    Returns
+    -------
+    Tuple with 3 elements:
+
+    start: int
+        elements offset in the chunk to read from
+    nitems: int
+        number of elements to read in the chunk from start
+    partial_out_selection: list of slices
+        indices of a temporary empty array of size `Array._chunks` to assign
+        the decompressed data to after the partial read.
+
+    Notes
+    -----
+    An array is flattened when compressed with blosc, so this iterator takes
+    the wanted selection of an array and determines the wanted coordinates
+    of the flattened, compressed data to be read and then decompressed. The
+    decompressed data is then placed in a temporary empty array of size
+    `Array._chunks` at the indices yielded as partial_out_selection.
+    Once all the slices yielded by this iterator have been read, decompressed
+    and written to the temporary array, the wanted slice of the chunk can be
+    indexed from the temporary array and written to the out_selection slice
+    of the out array.
+"""
+print(docstr)      
+
 def build_zarr_dataset():
     """Create a zarr array and save it."""
     store = zarr.DirectoryStore('data/array.zarr')
-    z = zarr.zeros((60, 404, 802), chunks=(12, 80, 160), store=store, overwrite=True)
-    z[...] = 42.
+    z = zarr.zeros(dsize, chunks=dchunks, store=store, overwrite=True)
+    z[...] = dvalue
     zarr.save_array("example.zarr", z, compressor=None)
 
 
@@ -19,9 +66,9 @@ def h5py_chunk_slice_info():
     """Use h5py and zarr utility to get chunk/slice info."""
     buf = BytesIO()
     with h5py.File(buf, 'w') as fout:
-        fout.create_dataset('test', shape=(60, 404, 802),
-                            chunks=(12, 80, 160), dtype='f8')
-        fout['test'][:] = 42.
+        fout.create_dataset('test', shape=dsize,
+                            chunks=dchunks, dtype='f8')
+        fout['test'][:] = dvalue
     buf.seek(0)
 
     with h5py.File(buf, 'r') as fin:
@@ -32,20 +79,25 @@ def h5py_chunk_slice_info():
         print("H5Py stuffs")
         print("==================")
         print(f"Dataset number of chunks is {num_chunks}")
-        print("Analyzing first 3 chunks")
-        for j in range(3):
+        print("\nAnalyzing 0th and 5th chunks")
+        for j in [0, 5]:
             print(f"Chunk index {j}")
             si = ds_id.get_chunk_info(j)  # return a StoreInfo object
-            print("Blob (Slice) chunk offset", si.chunk_offset)
-            print("Blob (Slice) byte offset", si.byte_offset)
-            print("Blob (Slice) size", si.size)
+            print("Chunk index offset", si.chunk_offset)
+            print("Chunk byte offset", si.byte_offset)
+            print("Chunk size", si.size)
+
+        tot_size = ds.size * ds.dtype.itemsize
+        print(f"\nTotal chunks size {tot_size}")
 
         # slice this cake
+        print("\nNow looking at some slices:")
         data_slice = ds[0:2]  # get converted to an ndarray
-        print(f"Slice array shape {ds.shape}")
+        print(f"Slice Dataset[0:2] shape {data_slice.shape}")
+        # use zarr.indexing.PartialChunkIterator
         PCI = PartialChunkIterator((slice(0, 2, 2), ), ds.shape)
-        print(list(PCI))
-        print(PCI.arr_shape)
+        print("Slice offset and size:", list(PCI)[0][0], "and",
+              list(PCI)[0][1] * ds.dtype.itemsize)
         print("\n")
 
 
@@ -70,24 +122,42 @@ def zarr_chunk_slice_info():
     chunk_offsets = [range(0, s, c) for s, c in zip(shape, chunks)]
     print("Chunk offsets", [tuple(k) for k in chunk_offsets])
     print("Zarr number of chunks", len(list(itertools.product(*chunk_offsets))))
+    offsets = []  # index offsets
+    sels = []  # indices
+    ch_sizes = []  # chunk sizes
     for offset in itertools.product(*chunk_offsets):
-        print("Slice offset", offset)
+        offsets.append(offset)
         sel = tuple(slice(o, min(s, o + c))
                     for o, s, c in zip(offset, shape, chunks))
+        sels.append(sel)
         islice = ds[sel]
-        print(f"Slice indices {sel}")
         slice_size = islice.size * islice.dtype.itemsize
-        print(f"Slice size {slice_size}")
+        ch_sizes.append(slice_size)
+
+    print("\nAnalyzing 0th and 5th chunks")
+    for j in [0, 5]:
+        print(f"Chunk index {j}")
+        print("Chunk index offset", offsets[j])
+        print("Chunk position", sels[j])
+        print("Chunk size", ch_sizes[j])
+
 
     tot_size = ds.size * ds.dtype.itemsize
-    print(f"Total chunks size {tot_size}")
+    print(f"\nTotal chunks size {tot_size}")
 
     # slice this cake
-    data_slice = ds[0:2]  # get converted to an ndarray
-    print(f"Slice array shape {ds.shape}")
-    PCI = PartialChunkIterator((slice(0, 2, 2), ), ds.shape)
-    print(list(PCI))
-    print(PCI.arr_shape)
+    print("\nNow looking at some slices:")
+    data_slice = ds[0:2]  # zarr data slice
+    print(f"Slice Dataset[0:2] shape {data_slice.shape}")
+    PCI = PartialChunkIterator((slice(0, 2, 1), ), ds.shape)
+    print("Slice offset and size", list(PCI)[0][0], "and",
+          list(PCI)[0][1] * ds.dtype.itemsize)
+    print("\n")
+    data_slice = ds[4:7]  # zarr data slice
+    print(f"Slice Dataset[4:7] shape {data_slice.shape}")
+    PCI = PartialChunkIterator((slice(0, 2, 1), ), ds.shape)
+    print("Slice offset and size", list(PCI)[0][0], "and",
+          list(PCI)[0][1] * ds.dtype.itemsize)
     print("\n")
 
 
