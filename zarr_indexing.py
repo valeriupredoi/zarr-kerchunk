5bf24c7a8560371158ff9a946952470030656b66
Valeriu Predoi <valeriu.predoi@gmail.com>
2022-08-24 12:35:05 +0100

    getting there finally

--- zarr_indexing.py
+++ zarr_indexing.py
@@ -1,55 +1,62 @@
 import h5py
 import itertools
+import os
 import zarr
 
+from io import BytesIO
 from zarr.indexing import PartialChunkIterator
 
 
-print("\nGet some libs paths")
-print("=====================")
-print(f"Zarr library at: {zarr.__file__}")
-
-# CMIP6 files
-cmip6_test_file = r"/home/valeriu/climate_data/CMIP6/CMIP/MPI-M/MPI-ESM1-2-HR/historical/r1i1p1f1/Omon/sos/gn/v20190710/sos_Omon_MPI-ESM1-2-HR_historical_r1i1p1f1_gn_198001-198412.nc"
-
-
-def build_zarr_array():
-    """Create the zarr array."""
+def build_zarr_dataset():
+    """Create a zarr array and save it."""
     store = zarr.DirectoryStore('data/array.zarr')
-    z = zarr.zeros((60, 404, 802), chunks=(60, 404, 802), store=store, overwrite=True)
-    z[...] = 42
+    z = zarr.zeros((60, 404, 802), chunks=(12, 80, 160), store=store, overwrite=True)
+    z[...] = 42.
     zarr.save_array("example.zarr", z, compressor=None)
 
 
-with h5py.File(cmip6_test_file, 'r') as fout:
-    print(f"netcdf4 file {fout}")
-    print(f"Data file keys {list(fout.keys())}")
-    print("Variable is: sos")
-    ds = fout['sos']
-    print(f"Variable dataset {ds}")
-    print(f"Variable data shape {ds.shape}")
-    ds_id = ds.id
-    print(f"Dataset id is {ds_id}")
-    num_chunks = ds_id.get_num_chunks()
-    print(f"Dataset number of chunks is {num_chunks}")
-    print("Analyzing first 3 chunks")
-    for j in range(3):
-        print(f"Chunk index {j}")
-        si = ds_id.get_chunk_info(j)  # return a StoreInfo object
-        print("Blob (Slice) chunk offset", si.chunk_offset)
-        print("Blob (Slice) byte offset", si.byte_offset)
-        print("Blob (Slice) size", si.size)
+def h5py_chunk_slice_info():
+    """Use h5py and zarr utility to get chunk/slice info."""
+    buf = BytesIO()
+    with h5py.File(buf, 'w') as fout:
+        fout.create_dataset('test', shape=(60, 404, 802),
+                            chunks=(12, 80, 160), dtype='f8')
+        fout['test'][:] = 42.
+    buf.seek(0)
 
-    # slice this cake
-    data_slice = ds[0:2]  # ndarray
-    print(ds.shape)
-    PCI = PartialChunkIterator((slice(0, 2, 2), ), ds.shape)
-    print(list(PCI))
-    print(PCI.arr_shape)
+    with h5py.File(buf, 'r') as fin:
+        ds = fin['test']
+        ds_id = fin['test'].id
+        num_chunks = ds_id.get_num_chunks()
+        print("\n")
+        print("H5Py stuffs")
+        print("==================")
+        print(f"Dataset number of chunks is {num_chunks}")
+        print("Analyzing first 3 chunks")
+        for j in range(3):
+            print(f"Chunk index {j}")
+            si = ds_id.get_chunk_info(j)  # return a StoreInfo object
+            print("Blob (Slice) chunk offset", si.chunk_offset)
+            print("Blob (Slice) byte offset", si.byte_offset)
+            print("Blob (Slice) size", si.size)
 
-    # build_zarr_array()
+        # slice this cake
+        data_slice = ds[0:2]  # get converted to an ndarray
+        print(f"Slice array shape {ds.shape}")
+        PCI = PartialChunkIterator((slice(0, 2, 2), ), ds.shape)
+        print(list(PCI))
+        print(PCI.arr_shape)
+        print("\n")
 
+
+def zarr_chunk_slice_info():
+    """Use pure zarr insides to get chunk/slice info."""
+    zarr_dir = "./example.zarr"
+    if not os.path.isdir(zarr_dir):
+        build_zarr_dataset()
     ds = zarr.open("./example.zarr")
+    print("Zarr stuffs")
+    print("==================")
     print(f"Data file loaded by Zarr\n: {ds}")
     print(f"Info of Data file loaded by Zarr\n: {ds.info}")
     # print(f"Data array loaded by Zarr\n: {ds[:]}")
@@ -74,3 +81,21 @@ with h5py.File(cmip6_test_file, 'r') as fout:
 
     tot_size = ds.size * ds.dtype.itemsize
     print(f"Total chunks size {tot_size}")
+
+    # slice this cake
+    data_slice = ds[0:2]  # get converted to an ndarray
+    print(f"Slice array shape {ds.shape}")
+    PCI = PartialChunkIterator((slice(0, 2, 2), ), ds.shape)
+    print(list(PCI))
+    print(PCI.arr_shape)
+    print("\n")
+
+
+def main():
+    "Run the meat."""
+    h5py_chunk_slice_info()
+    zarr_chunk_slice_info()
+
+
+if __name__ == '__main__':
+    main()
